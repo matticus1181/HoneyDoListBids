d560a3eea60095e373dd2f172c31c9ea7b5c4cdf
Matt Harbers <matt.harbers@gmail.com>
2013-11-01 18:07:42 -0400

    initial commit

---
+++ HoneyDoListBids/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoneyDoListBids::Application.config.secret_key_base = 'a852bff6d3a0d7c116443621509761fb34813f52d7b3c60446a243abcf7a052435470599faad331cd367aeb295bd7e73cb7c4d8d7e2dabaee780bd0eb85a7e34'
---
+++ HoneyDoListBids/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HoneyDoListBids::Application.config.session_store :cookie_store, key: '_HoneyDoListBids_session'
