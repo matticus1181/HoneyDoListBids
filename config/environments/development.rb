da9829fff679437b8f88399aed4a4061add17490
Matt Harbers <matt.harbers@gmail.com>
2013-11-06 12:13:09 -0500

    hopefully fixing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ HoneyDoListBids::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  require 'dotenv'
+  Dotenv.load
 end
